ec747ea14bcddc5a63f411b69d9b89f237d691e6
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-06-24 18:39:23 +0300

    Initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+
+        <!-- search -->
+        <search :value="search" placeholder="Type username ..." @search="search = $event" />
+        <button @click="getRepos" class="btn btnPrimary">Search!</button>
+
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+  import search from "@/components/Search.vue"
+  export default {
+    components: {
+      search
+    },
+    data() {
+      return {
+        search: ''
+      }
+    },
+    methods:{
+      getRepos(){
+        console.log(`Get user ${this.search} repos`)
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  button{
+    margin-top: 40px;
+  }
+</style>
